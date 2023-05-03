c07b7da9afd793cccae182611876e2d45f122a0f
Manef-Ben-Mansour1 <mohamedmanef.benmansour@insat.ucar.tn>
2023-05-03 18:21:11 +0100

    Linked header, subnav and cards

--- public/script/coeur.js
+++
@@ -1,9 +0,0 @@
-let list=document.querySelectorAll(".coeur")
-
-for(let i=0;i<list.length;i++) {
-list[i].addEventListener("click",()=>{
-    if(list[i].classList.contains("rouge")){list[i].classList.remove("rouge")}
-    else {list[i].classList.add("rouge")}
-
-})
-}
