266c463665d26f71458f4dfc38a1bf46d05c1de1
Manef-Ben-Mansour1 <mohamedmanef.benmansour@insat.ucar.tn>
2023-05-04 15:06:18 +0100

    Updated footer items

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -8,6 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomePageController extends AbstractController
 {
+    
     #[Route('/homepage', name: 'app_home_page')]
     public function index(): Response
     {
@@ -15,4 +16,5 @@ class HomePageController extends AbstractController
             'controller_name' => 'HomePageController',
         ]);
     }
+
 }
