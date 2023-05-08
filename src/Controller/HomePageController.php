a5d9c8ac18943e884b1b1a97b2947e51266adf23
Manef-Ben-Mansour1 <mohamedmanef.benmansour@insat.ucar.tn>
2023-05-08 18:35:59 +0100

    linked cards with the database

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,18 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\ImagesLogement;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Logement;
 class HomePageController extends AbstractController
-{
-    
+{    private ObjectManager $manager;
+    private ObjectRepository $repository;
+    private ObjectRepository $repositoryImage;
+
+
+    public function __construct(private ManagerRegistry $doctrine)
+    {
+        $this->manager = $this->doctrine->getManager();
+        $this->repository = $this->manager->getRepository(Logement::class);
+        $this->repositoryImage = $this->manager->getRepository(ImagesLogement::class);
+
+    }
+
+    public function imageParId($id){
+        $images = $this->repositoryImage->findBy(['idLogement'=>$id]);
+            foreach ($images as $image)
+            {$image->setData(base64_encode(stream_get_contents($image->getData())));}
+
+
+        return $images;
+    }
+
+
+
     #[Route('/homepage', name: 'app_home_page')]
     public function index(): Response
-    {
-        return $this->render('home_page/index.html.twig', [
-            'controller_name' => 'HomePageController',
+    {$logement=$this->repository->findAll();
+        return $this->render('home_page/index.html.twig', ['controller' => $this,
+          'logements'=>$logement,
         ]);
     }
 
