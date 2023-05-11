4f21fb172b492608924f117f05b083ebe62e681f
AzizBenDhiab <mohamedaziz.bendhiab@insat.ucar.tn>
2023-05-11 02:39:31 +0100

    final commiiiiiiiiiit :D

--- src/Controller/PostItemFormController.php
+++ src/Controller/PostItemFormController.php
@@ -1,60 +1,125 @@
 <?php
 
 namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\ImagesLogement;
 use App\Entity\Logement;
+use App\Form\LogementType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Intl\Countries;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Persistence\ObjectRepository;
+
 class PostItemFormController extends AbstractController
 {
     private ObjectManager $manager;
     private ObjectRepository $repository;
-    public function  __construct(private ManagerRegistry $doctrine)
+
+    public function __construct(private ManagerRegistry $doctrine)
     {
         $this->manager = $this->doctrine->getManager();
         $this->repository = $this->doctrine->getRepository(Logement::class);
     }
 
+    /**
+     * @throws TransportExceptionInterface
+     */
     #[Route('/postItemForm', name: 'app_post_item_form')]
-    public function index(Request $request): Response
+    public function addLogement(Request $request): Response
     {
-        if ($request->isMethod('POST')) {
-             $logement =new logement();
-            dump($_POST);
-            $logement->setTypeLogement($_POST['typeLogement']);
-
-            $logement->setTypeEspace("Espace commun");
-
-            $logement->setNumLogement(5);
-            $logement->setRue("dcd");
-            $logement->setPays("ed");
-            $logement->setVille("mm");
-
-            $logement->setNbrChambres(4);
-            $logement->setNbrLits(2);
-            $logement->setNbrSdb(2);
-            $logement->setEquipements($_POST['equipement']);
-            $logement->addImagesLogement();
-
-            $logement->setTitre("ee");
-            $logement->setDescription("e");
-            $logement->setPrixNuité("12.00") ;
+        $logement = new Logement();
+        $form = $this->createForm(LogementType::class, $logement);
+        $form->handleRequest($request);
 
 
-            $logement->setIdCompte();
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $logement->setTypeLogement($form->get('TypeLogement')->getData());
+                $logement->setTypeEspace($form->get('TypeEspace')->getData());
+                $countryCode = $form->get('Pays')->getData();
+                $countryName = Countries::getName($countryCode);
+                $logement->setPays($countryName);
+                $eq = $form->get('Equipements')->getData();
+                $values = array_values($eq);
+                $equip = array("wifi" => 0,
+                    "television" => 0,
+                    "climatisation" => 0,
+                    "Chauffage" => 0,
+                    "parking" => 0,
+                    "cuisine" => 0,
+                    "laveLinge" => 0,
+                    "sècheLinge" => 0,
+                    "espaceTravail" => 0,
+                    "sècheCheveux" => 0,
+                    "recharge" => 0,
+                    "fer" => 0,
+                    "piscine" => 0,
+                    "jacuzzi" => 0,
+                    "litBebe" => 0,
+                    "salleSport" => 0,
+                    "barbecue" => 0,
+                    "petitDej" => 0,
+                    "cheminee" => 0,
+                    "detectionMonoxyde" => 0,
+                    "detectionFumee" => 0,
+                    "fumeur" => 0,
+                    "bordMer" => 0,
+                    "frontMer" => 0);
+                foreach ($values as $val) {
+                    $equip[$val] = 1;
+                }
+                $logement->setEquipements($equip);
+                $logement->setprix_nuite($form->get('prix_nuite')->getData());
+                $logement->setIdUser($this->getUser());
 
 
-            $this->manager->persist($logement);
-            $this->manager->flush();
+                $uploadedFiles = $request->files->get('logement')['images'];
 
-        }
+                foreach ($uploadedFiles as $file) {
+                    $filePath = $file->getPathname();
+
+                    // Read the file contents and convert to a binary string (Blob)
+                    $fileContent = fread(fopen($filePath, 'rb'), filesize($filePath));
+
+                        $imagesLogement = new ImagesLogement();
+                        $imagesLogement->setData($fileContent);
+                        $imagesLogement->setName('image de' . $logement->getTitre());
+                        $imagesLogement->setIdLogement($logement);
+                        $this->manager->persist($imagesLogement);
+                    }
+
+
+
+                $this->manager->persist($logement);
+
+                /*                $email = (new Email())
+                                    ->from('ymaraentreprise@gmail.com')
+                                    ->to('bendhiab.aziz2@gmail.com')
+                                    ->subject('Logement accepté')
+                                    ->html('<h1>Votre logement a été déposé avec succès!</h1>
+                                          <h3>Visitez votre espace personnel pour voir les réservations  </h3>');
+
+                                $mailer->send($email);*/
+
+
+                $this->manager->flush();
+                $this->addFlash('success', 'Votre logement est ajouté avec succés');
+                return $this->redirectToRoute('app_post_item_form');
+            }}
 
         return $this->render('post_item_form/index.html.twig', [
             'controller_name' => 'PostItemFormController',
+            'form' => $form->createView()
         ]);
     }
+
+
 }
---
+++ src/Form/LogementType.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Logement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\CurrencyType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class LogementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('prix_nuite', NumberType::class, [
+                'attr' => ['class' => 'form-control',
+                    'placeholder' => 'Prix'
+                ],
+                'mapped'=>false
+
+            ])
+            ->add('TypeLogement', ChoiceType::class,
+                [
+                    'choices' => [
+                        'Maison' => '0',
+                        'Appartement' => '1',
+                        "Maison d'hôtes" => '2',
+                        'Hotel' => '3',
+
+                    ],
+                    'choice_attr' => [
+                        'Maison' => ['class' => 'radio-control'],
+                        'Appartement' => ['class' => 'radio-control'],
+                        "Maison d'hôtes" => ['class' => 'radio-control'],
+                        'Hotel' => ['class' => 'radio-control'],
+                    ],
+                    'label' => 'Type du logement proposé',
+                    'expanded' => true,
+                    'multiple' => false,
+                    'mapped' => false,
+
+                ])
+            ->add('TypeEspace', ChoiceType::class,
+                [
+                    'choices' => [
+                        'Un logement entier' => '0',
+                        'Une chambre privée' => '1',
+                        'Une chambre partagée' => '2',
+                    ],
+                    'choice_attr' => [
+                        'Un logement entier' => [
+                            'selected' => 'true'
+                        ],
+                        'label' => "Type de l'espace proposé"
+                    ],
+                    'mapped' => false,
+
+                ])
+            ->add('NumLogement', IntegerType::class,
+                [
+                    'attr' => ['class' => 'form-control',
+                        'id' => 'numero',
+                        'min' => 0
+                    ]
+
+                ])
+            ->add('Rue', TextType::class,
+                [
+                    'attr' => ['class' => 'form-control',
+                        'placeholder' => 'Rue'
+                    ]
+                ])
+            ->add('Ville', TextType::class,
+                [
+                    'attr' => ['class' => 'form-control',
+                        'placeholder' => 'Ville'
+                    ]
+                ])
+            ->add('Pays', CountryType::class,
+                [
+                    'attr' => ['class' => 'form-control'
+                    ]
+                ])
+            ->add('NbrLits', IntegerType::class,
+                [
+                    'attr' => ['class' => 'form-control',
+                        'min' => 0,
+                        'max' => 70]
+                ])
+            ->add('Nbr_sdb', IntegerType::class, [
+                'attr' => ['class' => 'form-control',
+                    'min' => 0,
+                    'max' => 10]
+            ])
+            ->add('NbrChambres', IntegerType::class, [
+                'attr' => ['class' => 'form-control',
+                    'min' => 0,
+                    'max' => 60
+                ]
+            ])
+            ->add('Titre', TextType::class,
+                [
+                    'attr' => ['class' => 'form-control',
+                        'placeholder' => "Titre de l'annonce"
+                    ]
+                ])
+            ->add('Description', TextType::class,
+                [
+                    'attr' => ['class' => 'form-control',
+                        'placeholder' => "Description"
+                    ]
+                ])
+            ->add('Equipements', ChoiceType::class,
+                [
+                    'choices' => [
+                        "Wifi" => "wifi",
+                        "Télévision" => "television",
+                        "Climatisation" => "climatisation",
+                        "Chauffage" => "chauffage",
+                        "Parking" => "parking",
+                        "Cuisine" => "cuisine",
+                        "Lave-Linge" => "laveLinge",
+                        "Sèche-Linge" => "sècheLinge",
+                        "Espace de travail dedié" => "espaceTravail",
+                        "Sèche-cheveux" => "sècheCheveux",
+                        "Recharge véhicules électriques" => "recharge",
+                        "Fer à repasser" => "fer",
+                        "Piscine" => "piscine",
+                        "Jacuzzi" => "jacuzzi",
+                        "Lit pour bébé" => "litBebe",
+                        "Salle de sport" => "salleSport",
+                        "Barbecue" => "barbecue",
+                        "Petit déjeuner" => "petitDej",
+                        "Cheminée" => "cheminee",
+                        "Détecteur de monoxyde de carbone" => "detectionMonoxyde",
+                        "Détection de fumée" => "detectionFumee",
+                        "Logement fumeur" => "fumeur",
+                        "Bord de mer" => "bordMer",
+                        "Front de mer" => "frontMer"
+                    ],
+                    'choice_attr' => [
+                        "Wifi" => ['class' => 'radio-control'],
+                        "Télévision" => ['class' => 'radio-control'],
+                        "Climatisation" => ['class' => 'radio-control'],
+                        "Chauffage" => ['class' => 'radio-control'],
+                        "Parking" => ['class' => 'radio-control'],
+                        "Cuisine" => ['class' => 'radio-control'],
+                        "Lave-Linge" => ['class' => 'radio-control'],
+                        "Sèche-Linge" => ['class' => 'radio-control'],
+                        "Espace de travail dedié" => ['class' => 'radio-control'],
+                        "Sèche-cheveux" => ['class' => 'radio-control'],
+                        "Recharge véhicules électriques" => ['class' => 'radio-control'],
+                        "Fer à repasser" => ['class' => 'radio-control'],
+                        "Piscine" => ['class' => 'radio-control'],
+                        "Jacuzzi" => ['class' => 'radio-control'],
+                        "Lit pour bébé" => ['class' => 'radio-control'],
+                        "Salle de sport" => ['class' => 'radio-control'],
+                        "Barbecue" => ['class' => 'radio-control'],
+                        "Petit déjeuner" => ['class' => 'radio-control'],
+                        "Cheminée" => ['class' => 'radio-control'],
+                        "Détecteur de monoxyde de carbone" => ['class' => 'radio-control'],
+                        "Détection de fumée" => ['class' => 'radio-control'],
+                        "Logement fumeur" => ['class' => 'radio-control'],
+                        "Bord de mer" => ['class' => 'radio-control'],
+                        "Front de mer" => ['class' => 'radio-control'],
+                    ],
+
+                    'expanded' => true,
+                    'multiple' => true,
+                    'mapped' => false
+                ])
+            ->add('images', FileType::class,
+                ['data_class' => null,
+
+                    'attr' => ['class' => 'form-control',
+                        "multiple" => true
+                    ],
+                    'multiple' => true,
+                    'mapped' => false
+
+                ])
+            ->add('Valider', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-primary']
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Logement::class,
+        ]);
+    }
+}
